9ae34a8571ab21859fa20271a3b842838678eef4
Dog429436 <99732803+Dog429436@users.noreply.github.com>
2024-11-14 23:02:48 +0200

    Delete numeric_operations.h

--- numeric_operations.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "math_parser.h"
-
-int getPrecedence(char sign);//טענת כניסה: קריאה לפעולה והכנסת תו, טענת יציאה: החזרת ערך העדיפות שלו
-
-bool hasHigherOrEqualPrecedence(tokens stackToken, tokens currentToken);//טענת כניסה: קריאה לפעולה עם טוקן נוכחי וטוקן במחסנית, טענת יציאה: החזרת אמת או שקר אם הטוקנים חוקיים ושהטוקן במחסנית בעל עדיפות גבוה או שווה לטוקן הנוכחי
-
-
